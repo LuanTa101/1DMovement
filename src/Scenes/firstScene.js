d71eea581d6623d151c8884f369add3b76990d47
Luan Ta <luta@ucsc.edu>
2024-04-24 16:53:23 -0700

    initial commit?

---
+++ src/Scenes/firstScene.js
@@ -0,0 +1,12 @@
+class Avatar extends Phaser.Scene {
+    constructor() {
+        super("first");
+    }
+ 
+     preload() {} // Load assets, called once
+ 
+     create() {}  // Create game objects, called once
+ 
+     update() {}  // Called N times/second
+ }
+ 
